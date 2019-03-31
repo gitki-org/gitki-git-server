444f3f6a64949ac222dcd21690995bde6a8a8b82
eldeni <eldeniyenden@gmail.com>
2019-03-31 05:13:49 -0700

    chore: set GOOGLE env based on current working directory
    - This is in order to run the app in different devices.

--- internals/prestart.sh
+++ internals/prestart.sh
@@ -1,1 +1,4 @@
-export GOOGLE_APPLICATION_CREDENTIALS="/Users/mistock1706/work/gitki/gitki-git-server/auth-keys/gitki-236120-fa0223e6d35b.json"
+dir=$(pwd)
+echo "[sh] Current directory $dir"
+
+export GOOGLE_APPLICATION_CREDENTIALS="$dir/auth-keys/gitki-236120-fa0223e6d35b.json"
